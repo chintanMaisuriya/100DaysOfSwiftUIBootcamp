7b4d61396a3f7c1235710611fa99fb043be4be6a
Chintan Maisuriya <csmaisuriya91@gmail.com>
2024-04-13 23:11:16 +0530

    Added 'RockPaperScissor' milestone challenge code

---
+++ Milestones/Milestone1/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Chintan Maisuriya on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
